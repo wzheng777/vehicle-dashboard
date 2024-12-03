c4d082c1b107044baa9e14dd8dee16aedc6a61d3
Wei Zheng <weizheng777@gmail.com>
2024-12-03 13:38:14 -0800

    modify menuButton

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MenuIcon from '@mui/icons-material/Settings';
+import ViewCompactIcon from '@mui/icons-material/ViewCompact';
 import './MenuButton.css';
 
 const MenuButton = ({ onMenuClick }) => {
   return (
     <div className="menu-button" onClick={onMenuClick}>
-      <MenuIcon style={{ fontSize: 40 }} />
-      <p>View Menu</p>
+      <ViewCompactIcon style={{ fontSize: 40 }} />
+      {/* <p>View Menu</p> */}
     </div>
   );
 };
