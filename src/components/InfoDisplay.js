5414014bff28cc9a123491fbbc12d55dac92d11a
Wei Zheng <weizheng777@gmail.com>
2024-12-04 15:59:47 -0800

    update infodisplay component

--- src/components/InfoDisplay.js
+++ src/components/InfoDisplay.js
@@ -1,43 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import InfoDisplay from './InfoDisplay';
-import MotorSpeedSlider from './MotorSpeedSlider';
-import './MidRow.css';
-import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
-import ThermostatIcon from '@mui/icons-material/Thermostat';
-import SettingsIcon from '@mui/icons-material/Settings';
+import './InfoDisplay.css';
 
-const MidRow = ({ batteryPercentage, batteryTemperature, gearRatio, motorRPM }) => {
+
+const InfoDisplay = ({ batteryPercentage, batteryTemperature, gearRatio, motorRPM }) => {
   return (
 
     <div className="info-display">
-      <div className="info-item">
-          <SettingsIcon style={{ fontSize: 40 }} />
-          <p>{gearRatio}</p>
-        </div>
-        <div className="info-item">
-          <BatteryChargingFullIcon style={{ fontSize: 40 }} />
-          <p>{batteryPercentage}%</p>
-        </div>
-        <div className="info-item">
-          <ThermostatIcon style={{ fontSize: 40 }} />
-          <p>{batteryTemperature}°C</p>
-        </div>
-        <div className="info-item">
-          <ThermostatIcon style={{ fontSize: 40 }} />
-          <p>{motorRPM}</p>
-        </div>
+      <div className="info-item" id="gear-ratio">
+        <div className='info-value'>{gearRatio}</div>
+      </div>
+      <div className="info-item" id="battery-percentage">
+        <div className='info-value'>{batteryPercentage}%</div>
+      </div>
+      <div className="info-item" id="battery-temperature">
+        <div className='info-value'>{batteryTemperature}°C</div>
+      </div>
+      <div className="info-item" id="motor-rpm">
+        <div className='info-value'>{motorRPM}</div>
+      </div>
     </div>
 
   );
 };
 
-MidRow.propTypes = {
+InfoDisplay.propTypes = {
   batteryPercentage: PropTypes.number.isRequired,
   batteryTemperature: PropTypes.number.isRequired,
   gearRatio: PropTypes.number.isRequired,
 };
 
-export default MidRow;
+export default InfoDisplay;
 
 
